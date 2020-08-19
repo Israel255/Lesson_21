5e3d5c262c52a351e9b7b4e681f1eade15b3de2c
Israel255 <62959042+Israel255@users.noreply.github.com>
2020-08-19 18:07:05 +0300

    Add files via upload

---
+++ Lesson_21/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_21
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        private int _codan;
+        protected int _number_of_seats;
+
+        public Car()
+        {
+
+        }
+        public Car(string model, string brand, int year, string color, int codan, int number_of_seats)
+        {
+            Model = model;
+            Brand = brand;
+            Year = year;
+            Color = color;
+            _codan = codan;
+            _number_of_seats = number_of_seats;
+        }
+        public int GetCoden()
+        {
+            return _codan;
+        }
+        public int GetNumberOfSeats()
+        {
+            return _number_of_seats;
+        }
+        protected void CahngeNumberOfSeats(int seats)
+        {
+            _number_of_seats += seats;
+        }
+
+        public override string ToString()
+        {
+            return "Car Brand: " + Brand + " Car Model: " + Model + " Car Year: " + Year + " Car Color: " + Color;
+        }
+    }
+}
---
+++ Lesson_21/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Lesson_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Car honda = new Car("Civic", "Honda", 2010, "Gray", 8520, 5);
+            XmlSerializer car_xml = new XmlSerializer(typeof(Car));
+            //using (Stream file = new FileStream(@"C:\Users\USER\Desktop\עבודות\Lesson 21\car.xml", FileMode.Create))
+            //{
+              // car_xml.Serialize(file, honda);
+            //}
+            Car honda_civic;
+            using (Stream file = new FileStream(@"C:\Users\USER\Desktop\עבודות\Lesson 21\car.xml", FileMode.Open))
+            {
+                honda_civic = car_xml.Deserialize(file) as Car;
+            }
+            Console.WriteLine(honda_civic);
+
+          //  Car[] cars =
+            //{
+              //  new Car ("Corolla","Toyota",2007,"Blue",4645,5),
+                //new Car ("Ibiza","Seat",2010,"Silver",1614,4),
+                //new Car ("Cx5","Mazda",2012,"Black",7913,7)
+            //};
+            XmlSerializer cars_xml = new XmlSerializer(typeof(Car[]));
+            //using (Stream cars_file = new FileStream(@"C:\Users\USER\Desktop\עבודות\Lesson 21\cars.xml",FileMode.Create))
+            //{
+            //  cars_xml.Serialize(cars_file, cars);
+            //}
+            Car[] cars_israel;
+            using (Stream cars_file = new FileStream(@"C:\Users\USER\Desktop\עבודות\Lesson 21\cars.xml", FileMode.Open))
+            {
+                cars_israel = cars_xml.Deserialize(cars_file) as Car[];
+            }
+            foreach (Car c in cars_israel)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
